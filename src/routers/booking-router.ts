08c726f31482d0f5fa6dc269b1573de5c1e4dd7d
RubsRafa <rubiarafaela2000@gmail.com>
2023-04-25 11:12:28 -0300

    feat: add booking files

---
+++ src/routers/booking-router.ts
@@ -0,0 +1,6 @@
+import { Router } from 'express';
+import { authenticateToken } from '@/middlewares';
+
+const bookingRouter = Router();
+
+bookingRouter.all('/*', authenticateToken).get('/').post('/').put('/:bookingId');
